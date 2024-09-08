ee4d8920edd1902149deff61fa312690571a5d10
cherishegoyibo <cherishegoyibo@gmail.com>
2024-09-08 13:34:22 +0100

    update

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Displays an employee TODO list progress """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    :
---
+++ 0x15-api/README.md
@@ -0,0 +1,2 @@
+ # 0x15. API
+This directory contains task completed for project 0x15. API
