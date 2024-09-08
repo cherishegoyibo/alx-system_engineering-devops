1d66e81c271e0681b12b79d569184aaea5e6b5b8
cherishegoyibo <cherishegoyibo@gmail.com>
2024-09-08 07:46:33 -0700

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,16 +12,19 @@ def number_of_subscribers(subreddit):
     (not active users, total subscribers) for a given subreddit.
     """
 
-    if subreddit is None or not isinstance(subreddit, str):
+    if not subreddit or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    headers = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
 
     try:
-        return results.get('data').get('subscribers')
+        response = get(url, headers=headers, timeout=10)
+	if response.status_code != 200:
+	return 0
 
-    except Exception:
+	data = response.json().get('data', {})
+	return data.get('subscribers', 0)
+
+    except Exception as e:
         return 0
